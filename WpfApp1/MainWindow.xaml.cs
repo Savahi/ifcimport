169529db666a628ac0be789dc722699817bc8df1
savahi <shirshhorn@gmail.com>
2020-11-23 23:00:17 +0300

    the first commit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.ComponentModel;
+using Microsoft.Win32;
+
+namespace WpfApp1
+{
+    public class Node : INotifyPropertyChanged
+    {
+        public ObservableCollection<Node> Nodes { get; set; }
+
+        public int Level { get; set; }
+        public string GlobalId { get; set; }
+        public string Name { get; set; }
+        public string TypeName { get; set; }
+        public string Volume { get; set; }
+
+        private bool isExpanded;
+        public bool IsExpanded {
+            get { return isExpanded; }
+            set
+            {
+                if (value != this.isExpanded)
+                {
+                    this.isExpanded = value;
+                    NotifyPropertyChanged("IsExpanded");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Node> _nodes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbLevels_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cmb = sender as ComboBox;
+            expandToLevel(cmb.SelectedIndex + 1, _nodes);
+        }
+
+        private void tvIFC_Expanded(object sender, RoutedEventArgs e)
+        {
+            if (tvIFC.SelectedItem != null)
+                (tvIFC.SelectedItem as Node).IsExpanded = !(tvIFC.SelectedItem as Node).IsExpanded;
+        }
+
+        private void expandToLevel( int maxLevel, ObservableCollection<Node> nodes)
+        {
+            foreach(var node in nodes)
+            {
+                if (node.Level < maxLevel) {
+                    node.IsExpanded = true;
+                }
+                else {
+                    node.IsExpanded = false;
+                }
+                if (node.Nodes != null) {
+                    expandToLevel(maxLevel, node.Nodes);
+                }
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true || true)
+            {
+                _nodes = new ObservableCollection<Node>();
+                int maxLevel=1;
+                Ifc.parseFile(openFileDialog.FileName, ref _nodes, ref maxLevel);
+                tvIFC.ItemsSource = _nodes;
+                expandToLevel(1, _nodes);
+
+                for (int i = 1; i <= maxLevel; i++)
+                    cmbLevels.Items.Add(new ComboBoxItem { Content = i.ToString() });
+                cmbLevels.SelectedIndex = 0;
+            }
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ WpfApp1/ifc.cs
@@ -0,0 +1,194 @@
+﻿using System.Linq;
+using Xbim.Ifc;
+using Xbim.Ifc4.Interfaces;
+using System.Collections.ObjectModel;
+
+namespace WpfApp1
+{
+    class Ifc
+    {
+        public static int parseFile(string fileName, ref ObservableCollection<Node> nodes, ref int maxLevel)
+        {
+            int r = -1;
+            using (IfcStore model = IfcStore.Open(fileName))
+            {
+                var project = model.Instances.FirstOrDefault<IIfcProject>();
+                readHierarchy(ref nodes, project, 1, ref maxLevel);
+                r = 0;
+            }
+            return r;
+        }
+
+
+        private static void readHierarchy(ref ObservableCollection<Node> nodes, IIfcObjectDefinition o, 
+            int level, ref int maxLevel)
+        {
+            if (level > maxLevel)
+                maxLevel = level;
+
+            string volume = "";
+            var oProduct = o as IIfcProduct;
+            if (oProduct != null)
+            {
+                readProp(oProduct, out volume);
+            }
+            Node node = new Node {
+                Level = level, Name = o.Name, GlobalId = o.GlobalId,
+                TypeName = o.GetType().Name, Volume=volume 
+            };
+            nodes.Add(node);
+            ObservableCollection<Node> subnodes = new ObservableCollection<Node>();
+            node.Nodes = subnodes;
+
+            //only spatial elements can contain building elements 
+            var oSpatialElement = o as IIfcSpatialStructureElement;
+            if (oSpatialElement != null)
+            {
+                //using IfcRelContainedInSpatialElement to get contained elements 
+                var containedElements = oSpatialElement.ContainsElements.SelectMany(rel => rel.RelatedElements);
+                foreach (var element in containedElements)
+                {
+                    readProp(element, out volume);
+                    subnodes.Add( new Node { Level=level+1, GlobalId=element.GlobalId, Name=element.Name,
+                        TypeName=element.GetType().Name, Volume=volume } );
+                }
+            }
+
+            //using IfcRelAggregates to get spatial decomposition of spatial structure elements 
+            foreach (var item in o.IsDecomposedBy.SelectMany(r => r.RelatedObjects))
+            {
+                readHierarchy(ref subnodes, item, level + 1, ref maxLevel);
+            }
+        } // End of readHierarchy
+
+
+        private static void readProp(IIfcProduct element, out string volume)
+        {
+            var properties = element.IsDefinedBy.
+                Where(r => r.RelatingPropertyDefinition is IIfcPropertySet).
+                SelectMany(r => ((IIfcPropertySet)r.RelatingPropertyDefinition).HasProperties).
+                OfType<IIfcPropertySingleValue>();
+            volume = "";
+            foreach (var property in properties)
+            {
+                //Console.WriteLine("Name=" + property.Name + ", value=" + property.NominalValue.ToString());
+                if ((property.Name).ToString().ToLower() == "volume")
+                {
+                    volume = property.NominalValue.ToString();
+                    return;
+                }
+            }
+            var properties2 = element.IsDefinedBy.
+                Where(r => r.RelatingPropertyDefinition is IIfcPropertySet).
+                SelectMany(r => ((IIfcPropertySet)r.RelatingPropertyDefinition).HasProperties).
+                OfType<IIfcPropertySingleValue>();
+            volume = "";
+            foreach (var property in properties2)
+            {
+                //Console.WriteLine("Name=" + property.Name + ", value=" + property.ToString());
+                if ((property.Name).ToString().ToLower() == "volume")
+                {
+                    volume = property.NominalValue.ToString();
+                    return;
+                }
+            }
+        } // End of readProp
+    }
+}
+
+
+/*
+            try
+            {
+                using (IfcStore model = IfcStore.Open(sourceFilePath))
+                {
+                    sourceFileOpened = true;
+
+                    // Creating a ".csv" file for importing operations into Spider Project
+                    using (FileStream opFile = File.Create(opFileName))
+                    {
+                        opFileCreated = true;
+                        using (StreamWriter opFileStreamWriter = new StreamWriter(opFile))
+                        {
+                            opFileStreamWriter.WriteLine("Level" + delimiter + "Code" + delimiter + "Name" +
+                                delimiter + "Type" + delimiter + "VolPlan" + delimiter + "f_Model");
+                            var project = model.Instances.FirstOrDefault<IIfcProject>();
+                            PrintHierarchy(opFileStreamWriter, project, 1);
+                        }
+                        opFile.Close();
+                        log("Success: " + opFileName + " created");
+                    }
+                    var context = new Xbim3DModelContext(model);
+                    context.CreateContext();
+
+                    // Creating a ".wexbim" one
+                    using (FileStream wexbimFile = File.Create(wexbimFileName))
+                    {
+                        using (BinaryWriter wexbimBinaryWriter = new BinaryWriter(wexbimFile))
+                        {
+                            model.SaveAsWexBim(wexbimBinaryWriter);
+                            wexbimBinaryWriter.Close();
+                        }
+                        wexbimFile.Close();
+                        wexbimCreated = true;
+                        log("Success: " + wexbimFileName + " created");
+                    }
+
+                    // Importing CoBie data into a ".json"...
+                    var facilities = new List<Facility>();
+                    
+                    var ifcToCoBieLiteUkExchanger = new IfcToCOBieLiteUkExchanger(model, facilities);
+                    facilities = ifcToCoBieLiteUkExchanger.Convert();
+
+                    var facilityType = facilities.FirstOrDefault();
+                    if (facilityType != null)
+                    {
+                        facilityType.WriteJson(jsonFileName, true);
+                        jsonCreated = true;
+                        log("Success: " + jsonFileName + " created");
+                        facilityType.WriteXml(xmlFileName, true);
+                        xmlCreated = true;
+                        log("Success: " + xmlFileName + " created");
+                        string errMsg;
+                        facilityType.WriteCobie("source.xls", out errMsg);
+                        if (errMsg.Length > 0)
+                            Console.WriteLine(errMsg);
+                        else 
+                            xlsCreated = true;
+                    }
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                string errorMessage = "An error occured!";
+                if (!sourceFileOpened)
+                {
+                    errorMessage += "\n  - Failed to open the source file " + sourceFilePath;
+                }
+                if (!opFileCreated)
+                {
+                    errorMessage += "\n  - Failed to create the destination file " + opFileName;
+                }
+                if (!wexbimCreated)
+                {
+                    log("\n - Failed to generate a geometry file " + wexbimFileName);
+                }
+                if (!jsonCreated)
+                {
+                    log("\n - Failed to generate a json file " + jsonFileName);
+                }
+                if (!xmlCreated)
+                {
+                    log("\n - Failed to generate an xml file " + xmlFileName);
+                }
+                if (!xlsCreated)
+                {
+                    log("\n - Failed to generate an xls file " + xlsFileName);
+                }
+                log(errorMessage);
+            }
+        }
+
+ 
+*/
